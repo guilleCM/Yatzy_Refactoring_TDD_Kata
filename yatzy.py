ab88991203721273d4853deea47ba258f23dd9a2
Guillermo <guille.mca@hotmail.com>
2017-01-12 19:24:27 +0100

    refactorizado metodo two_pairs

--- yatzy.py
+++ yatzy.py
@@ -79,24 +79,18 @@ class Yatzy:
 
     
     @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
+    def two_pair(*dice):
         score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
+        pairsFound = 0
+        for number in range(1, 7):
+            if pairsFound == 2:
+                return score
+            if dice.count(number) >= 4:
+                return number * 4
+            elif dice.count(number) >= 2:
+                score += number * 2
+                pairsFound += 1
+        return 0
     
     @staticmethod
     def four_of_a_kind( _1,  _2,  d3,  d4,  d5):

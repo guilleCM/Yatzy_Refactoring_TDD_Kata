e148f6a2f56520faf49136025f293bdb9bbe4ad9
Guillermo <guille.mca@hotmail.com>
2017-01-13 13:08:03 +0100

    codigo formateado a PEP8

--- test_yatzy_from_scratch.py
+++ test_yatzy_from_scratch.py
@@ -11,6 +11,7 @@ def test_chance():
     assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
     assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
 
+
 def test_yatzyScore():
     '''
     If all dice have the same number,
@@ -19,8 +20,9 @@ def test_yatzyScore():
         1,1,1,1,1 placed on "yatzy" scores 50
         1,1,1,2,1 placed on "yatzy
     '''
-    assert 50 == Yatzy.yatzyScore(1,1,1,1,1)
-    assert 0 == Yatzy.yatzyScore(1,1,1,2,1)
+    assert 50 == Yatzy.yatzyScore(1, 1, 1, 1, 1)
+    assert 0 == Yatzy.yatzyScore(1, 1, 1, 2, 1)
+
 
 def test_ones():
     '''
@@ -29,7 +31,8 @@ def test_ones():
     For Example:
     -   1,1,1,4,4 placed on "ones" scores 3 (1+1+1)
     '''
-    assert 3 == Yatzy.ones(1,1,1,4,4)
+    assert 3 == Yatzy.ones(1, 1, 1, 4, 4)
+
 
 def test_twos():
     '''
@@ -38,7 +41,8 @@ def test_twos():
     For Example:
     -   2,2,2,4,4 placed on "twos" scores 6 (2+2+2)
     '''
-    assert 6 == Yatzy.twos(2,2,2,4,4)
+    assert 6 == Yatzy.twos(2, 2, 2, 4, 4)
+
 
 def test_threes():
     '''
@@ -47,7 +51,8 @@ def test_threes():
     For Example:
     -   2,2,3,3,3 placed on "threes" scores 9 (3+3+3)
     '''
-    assert 9 == Yatzy.threes(2,2,3,3,3)
+    assert 9 == Yatzy.threes(2, 2, 3, 3, 3)
+
 
 def test_score_pair():
     '''
@@ -59,10 +64,11 @@ def test_score_pair():
     -   3,3,3,4,1 scores 6 (3+3)
     -   3,3,3,3,1 scores 6 (3+3)
     '''
-    assert 8 == Yatzy.score_pair(3,3,3,4,4)
-    assert 12 == Yatzy.score_pair(1,1,6,2,6)
-    assert 6 == Yatzy.score_pair(3,3,3,4,1)
-    assert 6 == Yatzy.score_pair(3,3,3,3,1)
+    assert 8 == Yatzy.score_pair(3, 3, 3, 4, 4)
+    assert 12 == Yatzy.score_pair(1, 1, 6, 2, 6)
+    assert 6 == Yatzy.score_pair(3, 3, 3, 4, 1)
+    assert 6 == Yatzy.score_pair(3, 3, 3, 3, 1)
+
 
 def test_two_pair():
     '''
@@ -70,14 +76,15 @@ def test_two_pair():
     If there are two pairs of dice with the same number, the
     player scores the sum of these dice. 
     For example, when placed on "two pairs":
-      
+
     -   1,1,2,3,3 scores 8 (1+1+3+3)
     -   1,1,2,3,4 scores 0
     -   1,1,2,2,2 scores 6 (1+1+2+2)
     '''
-    assert 8 == Yatzy.two_pair(1,1,2,3,3)
-    assert 0 == Yatzy.two_pair(1,1,2,3,4)
-    assert 6 == Yatzy.two_pair(1,1,2,2,2)
+    assert 8 == Yatzy.two_pair(1, 1, 2, 3, 3)
+    assert 0 == Yatzy.two_pair(1, 1, 2, 3, 4)
+    assert 6 == Yatzy.two_pair(1, 1, 2, 2, 2)
+
 
 def test_three_of_a_kind():
     '''
@@ -88,9 +95,10 @@ def test_three_of_a_kind():
     -    3,3,4,5,6 scores 0
     -    3,3,3,3,1 scores 9 (3+3+3)
     '''
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,4,5)
-    assert 0 == Yatzy.three_of_a_kind(3,3,4,5,6)
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,3,1)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
+    assert 0 == Yatzy.three_of_a_kind(3, 3, 4, 5, 6)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 1)
+
 
 def test_four_of_a_kind():
     '''
@@ -101,20 +109,22 @@ def test_four_of_a_kind():
     -    2,2,2,5,5 scores 0
     -    2,2,2,2,2 scores 8 (2+2+2+2)
     '''
-    assert 8 == Yatzy.four_of_a_kind(2,2,2,2,5)
-    assert 0 == Yatzy.four_of_a_kind(1,1,2,3,4)
-    assert 8 == Yatzy.four_of_a_kind(2,2,2,2,2)
+    assert 8 == Yatzy.four_of_a_kind(2, 2, 2, 2, 5)
+    assert 0 == Yatzy.four_of_a_kind(1, 1, 2, 3, 4)
+    assert 8 == Yatzy.four_of_a_kind(2, 2, 2, 2, 2)
+
 
 def test_small_straight():
     '''
     When placed on "small straight", if the dice read
     "1,2,3,4,5" the player scores 15 (the sum of all the dice).
     -   1,2,3,4,5 scores 15 (1+2+3+4+5)
-    ''' 
-    assert 15 == Yatzy.small_straight(1,2,3,4,5)
-    assert 15 == Yatzy.small_straight(5,4,3,2,1)
-    assert 15 == Yatzy.small_straight(2,1,5,4,3)
-    assert 0 == Yatzy.small_straight(4,2,3,4,5)
+    '''
+    assert 15 == Yatzy.small_straight(1, 2, 3, 4, 5)
+    assert 15 == Yatzy.small_straight(5, 4, 3, 2, 1)
+    assert 15 == Yatzy.small_straight(2, 1, 5, 4, 3)
+    assert 0 == Yatzy.small_straight(4, 2, 3, 4, 5)
+
 
 def test_large_straight():
     '''
@@ -122,10 +132,11 @@ def test_large_straight():
     the player scores 20 (the sum of all the dice).
     -   2,3,4,5,6 scores 20 (2+3+4+5+6)
     '''
-    assert 20 == Yatzy.large_straight(2,3,4,5,6)
-    assert 20 == Yatzy.large_straight(6,5,4,3,2)
-    assert 20 == Yatzy.large_straight(2,3,5,6,4)
-    assert 0 == Yatzy.large_straight(3,3,4,5,6)
+    assert 20 == Yatzy.large_straight(2, 3, 4, 5, 6)
+    assert 20 == Yatzy.large_straight(6, 5, 4, 3, 2)
+    assert 20 == Yatzy.large_straight(2, 3, 5, 6, 4)
+    assert 0 == Yatzy.large_straight(3, 3, 4, 5, 6)
+
 
 def test_full_house():
     '''
@@ -136,10 +147,11 @@ def test_full_house():
     -    2,2,3,3,4 scores 0
     -    4,4,4,4,4 scores 0
     '''
-    assert 8 == Yatzy.full_house(1,1,2,2,2)
-    assert 0 == Yatzy.full_house(2,2,3,3,4)
-    assert 0 == Yatzy.full_house(4,4,4,4,4)
-    assert 11 == Yatzy.full_house(3,3,1,1,3)
+    assert 8 == Yatzy.full_house(1, 1, 2, 2, 2)
+    assert 0 == Yatzy.full_house(2, 2, 3, 3, 4)
+    assert 0 == Yatzy.full_house(4, 4, 4, 4, 4)
+    assert 11 == Yatzy.full_house(3, 3, 1, 1, 3)
+
 
 @pytest.fixture
 def inyector():
@@ -154,6 +166,7 @@ def test_fours(inyector):
     # los metodos estaticos como chance()
     assert 4 == inyector.fours()
 
+
 def test_fives(inyector):
     '''
     # Fives: 
@@ -162,6 +175,7 @@ def test_fives(inyector):
     -   1,2,3,4,5 placed on "fives" scores 5 (5)
     '''
     assert 5 == inyector.fives()
+
 
 def test_sixs(inyector):
     '''
--- yatzy.py
+++ yatzy.py
@@ -4,7 +4,7 @@ class Yatzy:
     def chance(*dice):
         score = 0
         for die in dice:
-            score+=die
+            score += die
         return score
 
     @staticmethod
@@ -13,31 +13,31 @@ class Yatzy:
             if die != dice[0]:
                 return 0
         return 50
-    
+
     @staticmethod
     def ones(*dice):
         score = 0
         for die in dice:
-            if die==1:
-                score+=die
+            if die == 1:
+                score += die
         return score
-    
+
     @staticmethod
     def twos(*dice):
         score = 0
         for die in dice:
-            if die==2:
-                score+=die
+            if die == 2:
+                score += die
         return score
-    
+
     @staticmethod
     def threes(*dice):
         score = 0
         for die in dice:
-            if die==3:
-                score+=die
+            if die == 3:
+                score += die
         return score
-    
+
     def __init__(self, d1, d2, d3, d4, _5):
         self.dice = [0]*5
         self.dice[0] = d1
@@ -45,33 +45,33 @@ class Yatzy:
         self.dice[2] = d3
         self.dice[3] = d4
         self.dice[4] = _5
-    
+
     def fours(self):
         score = 0
         for die in self.dice:
-            if die==4:
-                score+=die
+            if die == 4:
+                score += die
         return score
-    
+
     def fives(self):
         score = 0
         for die in self.dice:
-            if die==5:
-                score+=die
+            if die == 5:
+                score += die
         return score
 
     def sixs(self):
         score = 0
         for die in self.dice:
-            if die==6:
-                score+=die
+            if die == 6:
+                score += die
         return score
-    
+
     @staticmethod
     def score_pair(*dice):
         PAIR = 2
-        for number in range(6,0,-1):
-            if dice.count(number)>=PAIR:
+        for number in range(6, 0, -1):
+            if dice.count(number) >= PAIR:
                 return PAIR * number
         return 0
 
@@ -88,40 +88,40 @@ class Yatzy:
                 score += number * 2
                 pairsFound += 1
         return 0
-    
+
     @staticmethod
     def three_of_a_kind(*dice):
         THREE = 3
-        for number in range(6,0,-1):
-            if dice.count(number)>=THREE:
+        for number in range(6, 0, -1):
+            if dice.count(number) >= THREE:
                 return THREE * number
         return 0
 
     @staticmethod
     def four_of_a_kind(*dice):
         FOUR = 4
-        for number in range(6,0,-1):
-            if dice.count(number)>=FOUR:
+        for number in range(6, 0, -1):
+            if dice.count(number) >= FOUR:
                 return FOUR * number
         return 0
-    
+
     @staticmethod
     def small_straight(*dice):
-        for number in range (1,6):
+        for number in range(1, 6):
             if dice.count(number) != 1:
                 return 0
         return 15
 
     @staticmethod
     def large_straight(*dice):
-        for number in range (2,7):
+        for number in range(2, 7):
             if dice.count(number) != 1:
                 return 0
-        return 20    
+        return 20
 
     @staticmethod
     def full_house(*dice):
-        score=0
+        score = 0
         pairFound = 0
         threeFound = 0
         for number in range(6):
